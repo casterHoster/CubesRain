f3fc296facffe0fa47665b2f0887581c6cba368b
casterHoster <95349741+casterHoster@users.noreply.github.com>
2024-07-02 21:54:10 +0300

    commit

--- CubesRain/Assets/Scripts/Cube.cs
+++ CubesRain/Assets/Scripts/Cube.cs
@@ -2,7 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(BoxCollider))]
+
+
 public class Cube : MonoBehaviour
 {
-    
+    [SerializeField] private List<Material> _materials;
+
+    private Renderer _renderer;
+    private bool _isTouched;
+    private float _minTime = 2;
+    private float _maxTime = 5;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void SetActiveOn()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.TryGetComponent(out Platform platform) && _isTouched == false)
+        {
+            _isTouched = true;
+            SetRandomMaterial();
+            StartCoroutine(StartTimerToDestroy(SetTime()));
+        }
+    }
+
+    private void SetRandomMaterial()
+    {
+        _renderer.sharedMaterial = _materials[Random.Range(0, _materials.Count)];
+    }
+
+    private float SetTime()
+    {
+        return Random.Range(_minTime, _maxTime + 1);
+    }
+
+    private IEnumerator StartTimerToDestroy(float time)
+    {
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
 }
--- CubesRain/Assets/Scripts/Spawner.cs
+++ CubesRain/Assets/Scripts/Spawner.cs
@@ -1,17 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Cube _cubePrefab;
     [SerializeField] private float _delay;
 
+    private ObjectPool<Cube> _cubePool;
     private int _minXCoordinate = -20;
     private int _maxXCoordinate = 20;
     private int _minZCoordinate = -20;
     private int _maxZCoordinate = 20;
     private int _yCoordinate = 40;
+
+    private void Awake()
+    {
+        _cubePool = new ObjectPool<Cube>(
+        createFunc:  () => Instantiate(_cubePrefab), 
+        actionOnGet: (cube) => cube.SetActiveOn(),
+        actionOnDestroy: (cube) => Destroy(cube),
+        collectionCheck: true,
+        defaultCapacity: 100, maxSize: 100);
+    }
 
     private void Start()
     {
@@ -22,9 +33,14 @@ public class Spawner : MonoBehaviour
     {
         while (true)
         {
-            Cube newCube = Instantiate(_cubePrefab, new Vector3(Random.Range(_minXCoordinate, _maxXCoordinate), _yCoordinate, Random.Range(_minZCoordinate, _maxZCoordinate)), Quaternion.identity);
+            _cubePool.Get();
             yield return new WaitForSeconds (_delay);
         }
 
+    }
+
+    private Cube Instantiate(Cube _cubePrefab)
+    {
+        return Instantiate(_cubePrefab, new Vector3(Random.Range(_minXCoordinate, _maxXCoordinate), _yCoordinate, Random.Range(_minZCoordinate, _maxZCoordinate)), Quaternion.identity);
     }
 }
